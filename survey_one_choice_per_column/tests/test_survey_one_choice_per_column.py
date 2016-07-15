0bfc3f9df063813f00473d801b7a2be561f39bb7
tarteo <dennissluijk@outlook.com>
2016-07-15 17:16:05 +0200

    [ADD] Skeleton unit tests

---
+++ survey_one_choice_per_column/tests/test_survey_one_choice_per_column.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# © 2016 ONESTEiN BV (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.tests.common import TransactionCase
+
+
+class TestSurveyOneChoicePerColumn(TransactionCase):
+    def test_ideal_case(self):
+        pass
+    
+    def test_multi_answers_for_column(self):
+        pass
