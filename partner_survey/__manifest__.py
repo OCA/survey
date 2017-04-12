f0f51c125d7bd565d95f0481b20cd847eb18aa0f
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-04-12 14:54:41 +0200

    Migrate partner_survey to 10.0

--- partner_survey/__manifest__.py
+++ partner_survey/__manifest__.py
@@ -1,23 +1,21 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
-#                    Antonio Espinosa <antonioea@antiun.com>
-# Copyright 2016 Camptocamp SA - Damien Crier
+# Copyright 2016 Tecnativa S.L.
+# Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 {
     'name': "Partner Survey",
     'category': 'Marketing',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'depends': [
         'survey',
     ],
     'data': [
         'views/res_partner_view.xml'
     ],
-    'author': 'Tecnativa S. L., '
+    'author': 'Tecnativa, '
               'Camptocamp, '
               'Odoo Community Association (OCA)',
-    'website': 'http://www.antiun.com',
+    'website': 'http://www.tecnativa.com',
     'license': 'AGPL-3',
     'installable': True,
 }
