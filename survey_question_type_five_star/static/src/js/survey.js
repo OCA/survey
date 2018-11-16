87bf9426d891112bb6be4d400e353628034401bf
sbejaoui <souheil.bejaoui@acsone.eu>
2018-11-16 14:20:14 +0100

    [FIX] - Fix Answer range for mandatory question
    mandatory question should have answer between 1-5 for star_rate type

--- survey_question_type_five_star/static/src/js/survey.js
+++ survey_question_type_five_star/static/src/js/survey.js
@@ -1,38 +1,41 @@
 /* Copyright 2018 ACSONE SA/NV
 License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).*/
-odoo.define('survey_question_type_five_star.survey', function(require) {
-    'use strict';
-    $(document).ready(function() {
-        //Print Result case
-        var the_form = $('.js_surveyform');
+odoo.define("survey_question_type_five_star.survey", function () {
+    "use strict";
+    $(document).ready(function () {
+        // Print Result case
+        var the_form = $(".js_surveyform");
         var prefill_controller = the_form.attr("data-prefill");
         if (!the_form.length) {
             return $.Deferred().reject("DOM doesn't contain '.js_surveyform'");
         }
-        function prefill() {
+
+        function prefill () {
             if (!_.isUndefined(prefill_controller)) {
                 var prefill_def = $.ajax(prefill_controller, {
-                        dataType: "json"
-                    })
-                    .done(function(json_data) {
-                        _.each(json_data, function(value, key) {
-                            var input = the_form.find(".form-control[name=" + key + "]");
-                            $(input).parent().find("label").each(function(label_index) {
-                                if ((5 - label_index) <= value) {
-                                    $(this).addClass("checked");
-                                }
+                        dataType: "json"})
+                    .done(function (json_data) {
+                        _.each(json_data, function (value, key) {
+                            var input = the_form.find(
+                                ".form-control[name=" + key + "]");
+                            $(input).parent().find("label").each(
+                                function (label_index) {
+                                    if ((5 - label_index) <= value) {
+                                        $(this).addClass("checked");
+                                    }
                             });
                         });
                     })
-                    .fail(function() {
-                        console.warn("[survey Question Type Five Star] Unable to load prefill data");
+                    .fail(function () {
+                        console.warn(
+                            "[survey Question Five Star] Unable to prefill");
                     });
                 return prefill_def;
             }
         }
         prefill();
         // Answer Case
-        $(".rate > label").click(function() {
+        $(".rate > label").click(function () {
             var label_items = $(this).parent().find("label");
             var value = label_items.length - $(this).index();
             label_items.removeClass("checked");
