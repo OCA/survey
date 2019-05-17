a082cde0228bb464b5503a3a65432155a04b343d
ernesto <ernesto.tejeda87@gmail.com>
2019-05-17 10:50:51 -0400

    partner_survey: Migration to 12.0

--- partner_survey/__manifest__.py
+++ partner_survey/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Tecnativa S.L.
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "Partner Survey",
     'category': 'Marketing',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'depends': [
         'survey',
     ],
@@ -15,7 +14,7 @@
     'author': 'Tecnativa, '
               'Camptocamp, '
               'Odoo Community Association (OCA)',
-    'website': 'http://www.tecnativa.com',
+    'website': 'https://github.com/OCA/survey',
     'license': 'AGPL-3',
     'installable': True,
 }
