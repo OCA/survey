b2237a37d8094cc1645bfdc887c6970ed020167d
Stephan Keller <MiStK@gmx.de>
2020-05-05 06:27:08 +0200

    [IMP] partner_survey: black, isort, prettier

--- partner_survey/__manifest__.py
+++ partner_survey/__manifest__.py
@@ -2,19 +2,13 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': "Partner Survey",
-    'category': 'Marketing',
-    'version': '12.0.1.0.0',
-    'depends': [
-        'survey',
-    ],
-    'data': [
-        'views/res_partner_view.xml'
-    ],
-    'author': 'Tecnativa, '
-              'Camptocamp, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/survey',
-    'license': 'AGPL-3',
-    'installable': True,
+    "name": "Partner Survey",
+    "category": "Marketing",
+    "version": "12.0.1.0.0",
+    "depends": ["survey",],
+    "data": ["views/res_partner_view.xml"],
+    "author": "Tecnativa, " "Camptocamp, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/survey",
+    "license": "AGPL-3",
+    "installable": True,
 }
