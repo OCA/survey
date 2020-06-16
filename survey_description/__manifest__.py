c9e6fb211061797201a2808b6f44fe734143f1b5
remi-filament <30716308+remi-filament@users.noreply.github.com>
2020-06-16 11:41:21 +0200

    [ADD] survey_description module

---
+++ survey_description/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Le Filament (<https://le-filament.com>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Survey description field",
+    "summary": "Displays description fields for survey, page and question",
+    "version": "12.0.1.0.0",
+    "development_status": "Beta",
+    "website": "https://github.com/OCA/survey.git",
+    "author": "Le Filament, Odoo Community Association (OCA)",
+    "maintainers": ["remi-filament"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "survey",
+    ],
+    "data": [
+        "views/survey_views.xml",
+    ],
+}
---
+++ survey_description/readme/DESCRIPTION.rst
@@ -0,0 +1,10 @@
+[ This file must be max 2-3 paragraphs, and is required. ]
+
+This module displays the description fields on backend for :
+* surveys
+* pages
+* questions
+
+Note: these fields are present in code, displayed on front-end for survey (when defined) but not displayed on back-end for user to fill.
+A PR has been submitted to Odoo (https://github.com/odoo/odoo/pull/52329) to implement this in Odoo 12.0 code, but since survey module has been refactored in v13.0 it may never be merged, so this module was created.
+
