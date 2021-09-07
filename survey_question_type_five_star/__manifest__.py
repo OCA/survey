89a0ae8945f2d27e56cd8039519a5b5273988c55
Olga Marco <olga.marco@creublanca.es>
2021-09-07 12:51:24 +0200

    [MIG] survey_question_type_five_star

--- survey_question_type_five_star/__manifest__.py
+++ survey_question_type_five_star/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Survey five stars question type',
-    'summary': """
+    "name": "Survey five stars question type",
+    "summary": """
         This module add five stars rating as question type for survey page""",
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/survey',
-    'depends': ['survey'],
-    'data': ['views/survey_question.xml', 'views/survey_template.xml'],
-    'demo': [],
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/survey",
+    "depends": ["survey"],
+    "data": ["views/survey_question.xml", "templates/survey_template.xml"],
+    "demo": [],
 }
--- survey_question_type_five_star/models/survey_user_input_line.py
+++ survey_question_type_five_star/models/survey_user_input_line.py
@@ -1,37 +1,32 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, _
+from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
 
 class SurveyUserInputLine(models.Model):
 
-    _inherit = 'survey.user_input_line'
+    _inherit = "survey.user_input_line"
 
     @api.model
     def save_line_star_rate(self, user_input_id, question, post, answer_tag):
         vals = {
-            'user_input_id': user_input_id,
-            'question_id': question.id,
-            'survey_id': question.survey_id.id,
-            'skipped': False,
+            "user_input_id": user_input_id,
+            "question_id": question.id,
+            "survey_id": question.survey_id.id,
+            "skipped": False,
         }
         if answer_tag in post and post[answer_tag].strip():
-            vals.update(
-                {
-                    'answer_type': 'number',
-                    'value_number': float(post[answer_tag]),
-                }
-            )
+            value = float(post[answer_tag])
+            vals.update({"answer_type": "number", "value_number": value})
         else:
-            vals.update({'answer_type': None, 'skipped': True})
+            vals.update({"answer_type": None, "skipped": True})
         old_uil = self.search(
             [
-                ('user_input_id', '=', user_input_id),
-                ('survey_id', '=', question.survey_id.id),
-                ('question_id', '=', question.id),
+                ("user_input_id", "=", user_input_id),
+                ("survey_id", "=", question.survey_id.id),
+                ("question_id", "=", question.id),
             ]
         )
         if old_uil:
@@ -40,24 +35,17 @@ class SurveyUserInputLine(models.Model):
             old_uil.create(vals)
         return True
 
-    @api.multi
-    @api.constrains('question_id', 'answer_type', 'value_number')
+    @api.constrains("question_id", "answer_type", "value_number")
     def _check_star_rate_answer(self):
         for rec in self:
-            if rec.question_id.type == 'star_rate':
-                if rec.answer_type != 'number':
+            if rec.question_id.question_type == "star_rate":
+                if rec.answer_type != "number":
                     raise ValidationError(
                         _("Five stars rate question must have numeric answer")
                     )
-                if rec.question_id.constr_mandatory and not (
-                    0 < rec.value_number <= 5
-                ):
-                    raise ValidationError(
-                        _("Answer is not in the right range")
-                    )
+                if rec.question_id.constr_mandatory and not (0 < rec.value_number <= 5):
+                    raise ValidationError(_("Answer is not in the right range"))
                 if not rec.question_id.constr_mandatory and not (
                     0 <= rec.value_number <= 5
                 ):
-                    raise ValidationError(
-                        _("Answer is not in the right range")
-                    )
+                    raise ValidationError(_("Answer is not in the right range"))
