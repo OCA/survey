7451e4f41b787004af76fc4dfa003185c736af10
Olga Marco <olga.marco@creublanca.es>
2021-09-17 11:47:02 +0200

    [IMP] survey_question_type_five_star: black, isort, prettier

--- survey_question_type_five_star/static/src/js/survey.js
+++ survey_question_type_five_star/static/src/js/survey.js
@@ -1,8 +1,8 @@
 /* Copyright 2018 ACSONE SA/NV
 License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).*/
-odoo.define("survey_question_type_five_star.survey", function() {
+odoo.define("survey_question_type_five_star.survey", function () {
     "use strict";
-    $(document).ready(function() {
+    $(document).ready(function () {
         // Print Result case
         var the_form = $(".js_surveyform");
         var prefill_controller = the_form.attr("data-prefill");
@@ -15,22 +15,18 @@ odoo.define("survey_question_type_five_star.survey", function() {
                 var prefill_def = $.ajax(prefill_controller, {
                     dataType: "json",
                 })
-                    .done(function(json_data) {
-                        _.each(json_data, function(value, key) {
+                    .done(function (json_data) {
+                        _.each(json_data, function (value, key) {
                             var input = the_form.find(
                                 ".form-control[name=" + key + "]"
                             );
-                            if (
-                                !$(input)
-                                    .parent()
-                                    .hasClass("print_star_rate")
-                            ) {
+                            if (!$(input).parent().hasClass("print_star_rate")) {
                                 return;
                             }
                             $(input)
                                 .parent()
                                 .find("label")
-                                .each(function(label_index) {
+                                .each(function (label_index) {
                                     if (5 - label_index <= value) {
                                         $(this).addClass("checked fa-star");
                                         $(this).removeClass("fa-star-o");
@@ -38,7 +34,7 @@ odoo.define("survey_question_type_five_star.survey", function() {
                                 });
                         });
                     })
-                    .fail(function() {
+                    .fail(function () {
                         console.warn("[survey Question Five Star] Unable to prefill");
                     });
                 return prefill_def;
@@ -46,19 +42,14 @@ odoo.define("survey_question_type_five_star.survey", function() {
         }
         prefill();
         // Answer Case
-        $(".rate > label").click(function() {
-            var label_items = $(this)
-                .parent()
-                .find("label");
+        $(".rate > label").click(function () {
+            var label_items = $(this).parent().find("label");
             var value = label_items.length - $(this).index();
             label_items.removeClass("checked fa-star");
             label_items.addClass("fa-star-o");
             label_items.slice($(this).index()).addClass("checked fa-star");
             label_items.slice($(this).index()).removeClass("fa-star-o");
-            $(this)
-                .parent()
-                .find("input")
-                .val(value);
+            $(this).parent().find("input").val(value);
         });
     });
 });
