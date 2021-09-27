0b4a9555b808241cafd6a949142a34d640821204
Olga Marco <olga.marco@creublanca.es>
2021-09-27 11:35:13 +0200

    [MIG] survey_question_type_nps: Migration to 14.0

--- survey_question_type_nps/tests/test_survey.py
+++ survey_question_type_nps/tests/test_survey.py
@@ -1,6 +1,5 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-import random
 
 from odoo.exceptions import ValidationError
 
@@ -67,16 +66,17 @@ class TestSurvey(common.SurveyCase):
                 {
                     "title": "Test NPS Rate",
                     "page_id": self.page1.id,
-                    "question": "Q0",
                     "question_type": "nps_rate",
                     "validation_required": True,
                 }
             )
         )
         self.answer_tag1 = "{}_{}_{}".format(
-            self.survey1.id, self.page1.id, self.question1.id,
+            self.survey1.id,
+            self.page1.id,
+            self.question1.id,
         )
-        self._type_match["nps_rate"] = ("number", "value_number")
+        self._type_match["nps_rate"] = ("numerical_box", "value_numerical_box")
 
     def test_01_question_nps_rate_with_error_values(self):
         error_results = [
@@ -84,76 +84,27 @@ class TestSurvey(common.SurveyCase):
             ("-1", "Answer is not in the right range"),
             ("11", "Answer is not in the right range"),
         ]
-        msg = (
-            "Validation function for type numerical_box is unable to "
-            "notify if answer is violating the validation rules"
-        )
         for i in range(len(error_results)):
             self.assertEqual(
-                self.question1.validate_question(
+                self.question1.validate_nps_rate(
                     {self.answer_tag1: error_results[i][0]}, self.answer_tag1
                 ),
                 {self.answer_tag1: error_results[i][1]},
-                msg=msg,
             )
 
     def test_02_question_nps_rate_with_valid_values(self):
         for i in ("0", "6", "8"):
-            self.assertTrue(
-                self.env["survey.user_input_line"].save_line_nps_rate(
-                    user_input_id=self.user_input1.id,
-                    question=self.question1,
-                    post={self.answer_tag1: i},
-                    answer_tag=self.answer_tag1,
-                )
+            self.user_input1.save_lines(question=self.question1, answer=i)
+            self.assertEqual(
+                self.user_input1.user_input_line_ids.filtered(
+                    lambda r: r.question_id == self.question1
+                ).value_numerical_box,
+                float(i),
             )
 
     def test_03_question_nps_rate_with_constr_mandatory(self):
         self.question1.constr_mandatory = True
         with self.assertRaises(ValidationError):
-            self.env["survey.user_input_line"].save_line_nps_rate(
-                user_input_id=self.user_input1.id,
-                question=self.question1,
-                post={self.answer_tag1: "0"},
-                answer_tag=self.answer_tag1,
-            )
+            self.user_input1.save_lines(question=self.question1, answer="0")
         with self.assertRaises(ValidationError):
-            self.env["survey.user_input_line"].save_line_nps_rate(
-                user_input_id=self.user_input1.id,
-                question=self.question1,
-                post={self.answer_tag1: "11"},
-                answer_tag=self.answer_tag1,
-            )
-
-    def test_04_question_nps_rate_with_error_values_and_mandatory(self):
-        self.question1.constr_mandatory = True
-        self.assertEqual(
-            self.question1.validate_question({self.answer_tag1: "0"}, self.answer_tag1),
-            {self.answer_tag1: self.question1.constr_error_msg},
-        )
-
-    def test_05_question_nps_rate_no_numerical(self):
-        with self.assertRaises(ValidationError):
-            self.env["survey.user_input_line"].save_line_nps_rate(
-                user_input_id=self.user_input1.id,
-                question=self.question1,
-                post={self.answer_tag1: ""},
-                answer_tag=self.answer_tag1,
-            )
-
-    def test_survey(self):
-        num = [float(n) for n in random.sample(range(1, 10), 3)]
-        nsum = sum(num)
-        for i in range(3):
-            answer = self._add_answer(self.survey1, False, email="public@example.com")
-            self._add_answer_line(self.question1, answer, str(num[i]))
-        exresult = {
-            "average": round((nsum / len(num)), 2),
-            "max": round(max(num), 2),
-            "min": round(min(num), 2),
-            "sum": nsum,
-        }
-        result = self.env["survey.survey"].prepare_result(self.question1)
-        self.assertTrue(result)
-        for key in exresult:
-            self.assertEqual(result[key], exresult[key])
+            self.user_input1.save_lines(question=self.question1, answer="11")
