65fd7102f98514e216cbe6c88f4c8b9081941484
Olga Marco <olga.marco@creublanca.es>
2021-09-27 11:35:13 +0200

    [MIG] survey_question_type_nps: Migration to 14.0

--- survey_question_type_nps/static/src/js/survey.js
+++ survey_question_type_nps/static/src/js/survey.js
@@ -1,63 +1,32 @@
 /* Copyright 2018 ACSONE SA/NV
 License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).*/
-odoo.define("survey_question_type_nps.survey", function() {
+odoo.define("survey_question_type_nps", function (require) {
     "use strict";
-    $(document).ready(function() {
-        // Print Result case
-        var the_form = $(".js_surveyform");
-        var prefill_controller = the_form.attr("data-prefill");
-        if (!the_form.length) {
-            return $.Deferred();
-        }
-
-        function prefill() {
-            if (!_.isUndefined(prefill_controller)) {
-                var prefill_def = $.ajax(prefill_controller, {
-                    dataType: "json",
-                })
-                    .done(function(json_data) {
-                        _.each(json_data, function(value, key) {
-                            var input = the_form.find(
-                                ".form-control[name=" + key + "]"
-                            );
-                            if (
-                                !$(input)
-                                    .parent()
-                                    .hasClass("print_nps_rate")
-                            ) {
-                                return;
-                            }
-                            $(input)
-                                .parent()
-                                .find("label")
-                                .each(function(label_index) {
-                                    if (10 - label_index === parseFloat(value[0])) {
-                                        $(this).addClass("checked");
-                                    }
-                                });
-                        });
-                    })
-                    .fail(function() {
-                        console.warn("[survey Question NPS] Unable to prefill");
-                    });
-                return prefill_def;
+    var SurveyFormWidget = require("survey.form");
+    SurveyFormWidget.include({
+        events: _.extend({}, SurveyFormWidget.prototype.events, {
+            "click .rate > label": "_onClickNPSLabel",
+        }),
+        _onClickNPSLabel: function (event) {
+            if (this.readonly) {
+                return;
             }
-        }
-        prefill();
-        // Answer Case
-        $(".rate > label").click(function() {
-            var label_items = $(this)
-                .parent()
-                .find("label");
-            var value = label_items.length - $(this).index();
+            var target = event.target;
+            var label_items = $(target).parent().find("label");
+            var value = label_items.length - $(target).index();
             label_items.removeClass("checked");
-            $(this).addClass("checked");
-            var input = $(this)
-                .parent()
-                .find("input");
-            input.val(value);
-            // We need to trigger the click in order to ensure the conditional usage. It is the easier workaround
-            input.trigger("click");
-        });
+            $(target).addClass("checked");
+            $(target).parent().find("input").val(value);
+        },
+        _prepareSubmitValues: function (formData, params) {
+            this._super.apply(this, arguments);
+            this.$("[data-question-type]").each(function () {
+                switch ($(this).data("questionType")) {
+                    case "nps_rate":
+                        params[this.name] = this.value;
+                        break;
+                }
+            });
+        },
     });
 });
