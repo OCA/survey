7bf8e4decf44a4362815126b99a8a31c719994ee
Olga Marco <olga.marco@creublanca.es>
2021-11-05 09:58:54 +0100

    [IMP] survey_description: black, isort, prettier

--- survey_description/__manifest__.py
+++ survey_description/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": """
                Displays description and thank you fields for survey,
                page and question""",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "development_status": "Beta",
     "website": "https://github.com/OCA/survey.git",
     "author": "Le Filament, Odoo Community Association (OCA)",
@@ -13,10 +13,6 @@
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "survey",
-    ],
-    "data": [
-        "views/survey_views.xml",
-    ],
+    "depends": ["survey"],
+    "data": ["views/survey_views.xml"],
 }
