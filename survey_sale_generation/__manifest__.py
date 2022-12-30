d3d52c34841e08c20d31dcbe26c1f8b874bfa873
David <david.vidal@tecnativa.com>
2022-12-30 16:33:33 +0100

    [ADD] survey_sale_generation: New module 13.0
    TT40983

---
+++ survey_sale_generation/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Survey sale generation",
+    "summary": "Generate sale orders from surveys",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Marketing/Survey",
+    "website": "https://github.com/OCA/survey",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["chienandalu"],
+    "license": "AGPL-3",
+    "depends": ["survey", "sale"],
+    "data": ["views/survey_question_views.xml", "views/survey_survey_views.xml"],
+}
