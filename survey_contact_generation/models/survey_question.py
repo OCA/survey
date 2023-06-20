9657bb6e814117817d3c14de65a9bc8b1109e9bd
David <david.vidal@tecnativa.com>
2023-06-20 17:50:05 +0200

    [IMP] survey_contact_generation: multiple choice
    - Added support for multiple choice questions
    - We can choose many2one field values in multiple choice single
    - We can use many2many field values in multiple choice multiple
    - We can use whatever field in multiple choices
    - Now we can use the comment field as a container for any question info
      that doesn't fit into other fields.
    
    TT40985

--- survey_contact_generation/models/survey_question.py
+++ survey_contact_generation/models/survey_question.py
@@ -18,11 +18,13 @@ class SurveyQuestion(models.Model):
     @api.depends("question_type")
     def _compute_allowed_field_ids(self):
         type_mapping = {
-            "textbox": ["char"],
+            "textbox": ["char", "text"],
             "free_text": ["text"],
-            "numerical_box": ["integer", "float"],
-            "date": ["date"],
-            "datetime": ["datetime"],
+            "numerical_box": ["integer", "float", "text", "char"],
+            "date": ["date", "text", "char"],
+            "datetime": ["datetime", "text", "char"],
+            "simple_choice": ["many2one", "text", "char"],
+            "multiple_choice": ["many2many", "text", "char"],
         }
         for record in self:
             record.allowed_field_ids = (
@@ -35,3 +37,43 @@ class SurveyQuestion(models.Model):
                 )
                 .ids
             )
+
+
+class SurveyLabel(models.Model):
+    _inherit = "survey.label"
+
+    @api.model
+    def default_get(self, fields):
+        result = super().default_get(fields)
+        if (
+            not result.get("res_partner_field")
+            or "res_partner_field_resource_ref" not in fields
+        ):
+            return result
+        partner_field = self.env["ir.model.fields"].browse(result["res_partner_field"])
+        # Otherwise we'll just use the value (char, text)
+        if partner_field.ttype not in {"many2one", "many2many"}:
+            return result
+        res = self.env[partner_field.relation].search([], limit=1)
+        if res:
+            result["res_partner_field_resource_ref"] = "%s,%s" % (
+                partner_field.relation,
+                res.id,
+            )
+        return result
+
+    @api.model
+    def _selection_res_partner_field_resource_ref(self):
+        return [(model.model, model.name) for model in self.env["ir.model"].search([])]
+
+    res_partner_field = fields.Many2one(related="question_id.res_partner_field")
+    res_partner_field_resource_ref = fields.Reference(
+        string="Contact field value",
+        selection="_selection_res_partner_field_resource_ref",
+    )
+
+    @api.onchange("res_partner_field_resource_ref")
+    def _onchange_res_partner_field_resource_ref(self):
+        """Set the default value as the product name, although we can change it"""
+        if self.res_partner_field_resource_ref:
+            self.value = self.res_partner_field_resource_ref.display_name or ""
