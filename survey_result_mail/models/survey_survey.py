15b6357bf32e4ff0562c85ef5b8a4e07fda102bf
David <david.vidal@tecnativa.com>
2023-07-06 16:47:04 +0200

    [MIG] survey_result_mail: Migration to 15.0
    TT44285

--- survey_result_mail/models/survey_survey.py
+++ survey_result_mail/models/survey_survey.py
@@ -7,6 +7,10 @@ class SurveySurvey(models.Model):
     _inherit = "survey.survey"
 
     send_result_mail = fields.Boolean(
+        string="Send survey answers",
         help="When the survey is submitted, an email will be sent to the user with"
-        "the answers"
+        "the answers",
+    )
+    result_mail_template_id = fields.Many2one(
+        comodel_name="mail.template", domain="[('model', '=', 'survey.user_input')]"
     )
