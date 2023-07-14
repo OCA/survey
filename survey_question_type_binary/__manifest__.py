db914ba28fd782775048d2b09a72d6c09e31c386
Jose Zambudio <jose@aurestic.es>
2023-07-14 13:59:01 +0200

    [ADD][14.0] survey_question_type_binary

---
+++ survey_question_type_binary/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Jose Zambudio - Aures Tic <jose@aurestic.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Survey binary question type",
+    "summary": """
+        This module add binary field as question type for survey page""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Aures TIC, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/survey",
+    "depends": ["survey"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/survey_question.xml",
+        "views/survey_user_input_line.xml",
+        "templates/survey_template.xml",
+    ],
+}
