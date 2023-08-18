e0c0de4dc728dc0654037e26733b4c22b03b7027
David <david.vidal@tecnativa.com>
2023-08-18 07:48:59 +0200

    [ADD] survey_answer_generation: New module
    TT40984

---
+++ survey_answer_generation/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Survey answer generation",
+    "summary": "Link and compare answers from another survey",
+    "version": "15.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Marketing/Survey",
+    "website": "https://github.com/OCA/survey",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["chienandalu"],
+    "license": "AGPL-3",
+    "depends": ["survey"],
+    "data": [
+        "views/survey_question_views.xml",
+        "views/survey_survey_views.xml",
+        "views/survey_user_input_views.xml",
+    ],
+}
