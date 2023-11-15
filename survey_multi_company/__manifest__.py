d5acd16a21c38f461e3475275a7b8dd423aba924
David <david.vidal@tecnativa.com>
2023-11-15 16:37:18 +0100

    [ADD] survey_multi_company: New module
    TT44905

---
+++ survey_multi_company/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Survey company rules",
+    "summary": "Company security for surveys",
+    "version": "15.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Marketing/Survey",
+    "website": "https://github.com/OCA/survey",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["chienandalu"],
+    "license": "AGPL-3",
+    "depends": ["survey"],
+    "data": [
+        "views/survey_views.xml",
+        "security/survey_security.xml",
+    ],
+}
---
+++ survey_multi_company/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module allows to restrict surveys per company.
