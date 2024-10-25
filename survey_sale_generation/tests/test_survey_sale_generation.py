68bf71c3dea109588688254c198ec138238d7f7f
David <david.vidal@tecnativa.com>
2024-10-25 08:57:42 +0200

    [IMP] survey_sale_generation: sale template on answer
    Now we can use a single choice question to force the template into the
    generated sale order.
    
    TT51297

--- survey_sale_generation/tests/test_survey_sale_generation.py
+++ survey_sale_generation/tests/test_survey_sale_generation.py
@@ -8,8 +8,8 @@ from odoo.addons.survey.tests.common import SurveyCase
 @tagged("-at_install", "post_install")
 class SurveySaleGenerationCase(SurveyCase, HttpCase):
     def setUp(self):
-        """We run the tour in the setup so we can share the tests case with other
-        modules"""
+        # We run the tour in the setup so we can share the tests case with other
+        # modules
         super().setUp()
         self.support_hours = self.env.ref("survey_sale_generation.support_hours")
         self.gold_service = self.env.ref("survey_sale_generation.gold_service")
@@ -17,6 +17,12 @@ class SurveySaleGenerationCase(SurveyCase, HttpCase):
         self.mail_management = self.env.ref("survey_sale_generation.mail_management")
         self.support_hiring_team = self.env.ref("survey_sale_generation.support_hiring")
         self.survey = self.env.ref("survey_sale_generation.survey_hire_support")
+        self.quotation_template_1 = self.env["sale.order.template"].create(
+            {
+                "name": "Test order template 1",
+            }
+        )
+        self.survey.sale_order_template_id = self.quotation_template_1
         initial_user_inputs = self.survey.user_input_ids
         # Run the survey as a portal user and get the generated quotation
         self.start_tour(
@@ -55,7 +61,43 @@ class SurveySaleGenerationTests(SurveySaleGenerationCase):
         self.assertEqual(
             self.generated_sale.partner_id, self.env.ref("base.partner_demo_portal")
         )
+        self.assertEqual(
+            self.generated_sale.sale_order_template_id, self.quotation_template_1
+        )
         info_message, *_ = self.generated_sale.message_ids
         # Some other survey inputs can be annotated in the quotation chatter
         self.assertTrue("test@test.com" in info_message.body)
         self.assertEqual("Mr. Odoo", self.generated_sale.origin)
+
+
+@tagged("-at_install", "post_install")
+class SurveySaleGenerationQuotationTemplateTests(SurveySaleGenerationCase):
+    def setUp(self):
+        self.quotation_template_2 = self.env["sale.order.template"].create(
+            {
+                "name": "Test order template 2",
+            }
+        )
+        self.quotation_template_3 = self.env["sale.order.template"].create(
+            {
+                "name": "Test order template 3",
+            }
+        )
+        # Platinum forces template 2
+        self.env.ref(
+            "survey_sale_generation.survey_support_q2_sug1"
+        ).sale_order_template_id = self.quotation_template_2
+        # Gold forces template 3
+        self.env.ref(
+            "survey_sale_generation.survey_support_q2_sug2"
+        ).sale_order_template_id = self.quotation_template_3
+        # And the survey default is template 1
+        super().setUp()
+
+    def test_sale_template_generation(self):
+        self.assertEqual(
+            self.generated_sale.sale_order_template_id,
+            self.quotation_template_3,
+            "The answer to the subscription level was gold, which had quotation "
+            "template 3 as its value for the genarated sale",
+        )
