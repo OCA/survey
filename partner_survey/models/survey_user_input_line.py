83df539b11272f6b43d621b91f273ffe179b4229
Franco Leyes <lef@adhoc.com.ar>
2024-11-28 16:09:34 -0300

    [MIG] partner_survey: Migration to 18.0

--- partner_survey/models/survey_user_input_line.py
+++ partner_survey/models/survey_user_input_line.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class SurveyUserInputLine(models.Model):
     _inherit = "survey.user_input.line"
 
-    partner_id = fields.Many2one(related="user_input_id.partner_id", store=True)
+    partner_id = fields.Many2one(related="user_input_id.partner_id")
