ee44a6cf30994c2b2b797949f8c64e7a3ea89b7f
kobros-tech <alkobroslymohamed@gmail.com>
2025-04-13 06:18:20 +0300

    [IMP] survey_question_type_binary: sign app production mode

--- survey_question_type_binary/static/src/js/sign.esm.js
+++ survey_question_type_binary/static/src/js/sign.esm.js
@@ -4,29 +4,38 @@ import {registry} from "@web/core/registry";
 import {SignatureDialog} from "@web/core/signature/signature_dialog";
 import {renderToString} from "@web/core/utils/render";
 import {templates} from "@web/core/assets";
+import {_lt} from "@web/core/l10n/translation";
 import {App} from "@odoo/owl";
 
 export const surveySign = {
     dependencies: ["dialog"],
     async start(env, {dialog}) {
-        const app = new App(null, {templates, test: true});
+        const app = new App(null, {
+            templates,
+            dev: env.debug,
+            translatableAttributes: ["data-tooltip"],
+            translateFn: (term) => _lt(term),
+            test: false,
+        });
         renderToString.app = app;
         // Create and mount dummy app to errors from renderToString
         // which will not enable the signature dialog of auto type
-        const nameAndSignatureProps = {
+        const dialogProps = {
+            defaultName: "Write Your Name",
+            fontColor: "DarkBlue",
             mode: "auto",
             displaySignatureRatio: 3,
             signatureType: "signature",
-        };
-        const dialogProps = {
-            defaultName: "Write Your Name",
-            nameAndSignatureProps,
+            noInputName: false,
             uploadSignature: (data) => this.uploadSignature(data),
         };
         // Open signature dialog only if its button is mounted to DOM
         const waitForButton = () => {
             const btn = document.getElementById("survey_signature_btn_id");
             if (btn) {
+                if (btn.dataset.participantName) {
+                    dialogProps.defaultName = btn.dataset.participantName;
+                }
                 btn.addEventListener("click", () => {
                     dialog.add(SignatureDialog, dialogProps);
                 });
