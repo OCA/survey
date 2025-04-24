8e150ae328a0aefe5d5613238d4b7a1fe663f7e4
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2025-04-24 15:09:49 +0200

    [MIG] survey_multi_company: Migration to version 17.0
    TT51951

--- survey_multi_company/__manifest__.py
+++ survey_multi_company/__manifest__.py
@@ -3,12 +3,12 @@
 {
     "name": "Survey company rules",
     "summary": "Company security for surveys",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "development_status": "Beta",
     "category": "Marketing/Survey",
     "website": "https://github.com/OCA/survey",
     "author": "Tecnativa, Odoo Community Association (OCA)",
-    "maintainers": ["chienandalu"],
+    "maintainers": ["pilarvargas-tecnativa"],
     "license": "AGPL-3",
     "depends": ["survey"],
     "data": [
