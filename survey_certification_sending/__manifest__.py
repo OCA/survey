e907dccdd9280b37d2593b45f9d6a03c87600bab
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2025-05-14 18:39:53 +0200

    [MIG] survey_certification_sending: Migration to version 17.0
    TT56327

--- survey_certification_sending/__manifest__.py
+++ survey_certification_sending/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Survey Certification Sending",
     "summary": "Controls the automatic sending of certifications in surveys.",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "development_status": "Beta",
     "category": "Marketing/Survey",
