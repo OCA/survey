123b27e9ed694688be795ee9fdc32ef1068c9ee1
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2025-07-14 10:46:51 +0200

    [MIG] survey_answer_generation: Migration to version 17.0 TT51946

--- survey_answer_generation/__manifest__.py
+++ survey_answer_generation/__manifest__.py
@@ -3,12 +3,12 @@
 {
     "name": "Survey answer generation",
     "summary": "Link and compare answers from another survey",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "development_status": "Beta",
     "category": "Marketing/Survey",
     "website": "https://github.com/OCA/survey",
     "author": "Tecnativa, Odoo Community Association (OCA)",
-    "maintainers": ["chienandalu"],
+    "maintainers": ["pilarvargas-tecnativa"],
     "license": "AGPL-3",
     "depends": ["survey"],
     "data": [
