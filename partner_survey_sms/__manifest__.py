a7591847d7d341271fcbcf8b1b4ff07ea710e22f
kobros-tech <alkobroslymohamed@gmail.com>
2025-08-23 04:52:35 +0300

    [16.0][ADD] partner_survey_sms
    Send survey participation link via SMS

---
+++ partner_survey_sms/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2025 Kencove - Mohamed Alkobrosli (http://kencove.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Survey SMS",
+    "summary": "Send survey participation link via SMS",
+    "category": "Marketing/Surveys",
+    "version": "16.0.1.0.0",
+    "author": "Kencove, Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/survey",
+    "license": "AGPL-3",
+    "depends": [
+        "survey",
+        "sms",
+        "html_text",
+    ],
+    "data": [
+        "data/sms_template.xml",
+        "wizard/survey_invite_views.xml",
+    ],
+    "installable": True,
+}
