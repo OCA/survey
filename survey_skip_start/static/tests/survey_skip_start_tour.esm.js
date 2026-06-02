c9f6191633e22b51e1175f0a08473b00cdd291da
Laya Rabasa <lrabasa@elegosoft.com>
2026-06-02 10:17:40 +0000

    [MIG] survey_skip_start: Migration to 18.0

--- survey_skip_start/static/tests/survey_skip_start_tour.esm.js
+++ survey_skip_start/static/tests/survey_skip_start_tour.esm.js
@@ -3,13 +3,11 @@
 import {registry} from "@web/core/registry";
 
 registry.category("web_tour.tours").add("test_survey_skip_start", {
-    test: true,
-    url: "/survey/start/b135640d-14d4-4748-9ef6-344ca256531e",
     steps: () => [
         {
             content: "Answer Where do you live",
             trigger: 'div.js_question-wrapper:contains("Where do you live") input',
-            run: "text Spain",
+            run: "fill Spain",
         },
     ],
 });
