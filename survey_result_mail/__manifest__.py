457c420d5c56075531a5dda63517f86a8c8f7464
Laya Rabasa <lrabasa@elegosoft.com>
2026-06-17 16:48:14 +0000

    [MIG] survey_result_mail: Migration to 18.0

--- survey_result_mail/__manifest__.py
+++ survey_result_mail/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Survey Result Mail",
     "summary": "Send survey answers to the survey user",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "development_status": "Beta",
     "category": "Marketing/Survey",
     "website": "https://github.com/OCA/survey",
