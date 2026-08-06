1305aa0853edf4dd8306f8e7425d63e1940f19fe
Adasat Torres de León <adasat.torres@tecnativa.com>
2026-08-06 14:26:27 +0100

    [MIG] survey_skip_start: Migration to version 19.0

--- survey_skip_start/tests/test_survey_skip_start.py
+++ survey_skip_start/tests/test_survey_skip_start.py
@@ -1,5 +1,7 @@
 # Copyright 2024 Tecnativa - David Vidal
+# Copyright 2026 Tecnativa - Adasat Torres
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import Command
 from odoo.tests import HttpCase, tagged
 
 
@@ -9,13 +11,31 @@ class SurveySkipStartCase(HttpCase):
         """We run the tour in the setup so we can share the tests case with other
         modules"""
         super().setUp()
-        self.survey = self.env.ref("survey.survey_feedback")
+        self.survey = self.env["survey.survey"].create(
+            {
+                "title": "Survey Test",
+                "description": "A description test",
+                "questions_layout": "page_per_section",
+                "skip_start": True,
+                "question_and_page_ids": [
+                    Command.create(
+                        {"title": "Page Test", "is_page": True, "sequence": 1}
+                    ),
+                    Command.create(
+                        {
+                            "title": "Where do you live?",
+                            "question_type": "char_box",
+                            "is_page": False,
+                            "sequence": 2,
+                        }
+                    ),
+                ],
+            }
+        )
 
     def test_skip_survey_start_screen(self):
         """Skip the start screen"""
-        self.survey.skip_start = True
         self.start_tour(
             f"/survey/start/{self.survey.access_token}",
             "test_survey_skip_start",
-            step_delay=1000,
         )
