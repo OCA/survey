fd404ccb45922180b256607bf09a98962c6d0923
Adasat Torres de León <adasat.torres@tecnativa.com>
2026-08-12 10:51:32 +0100

    [MIG] survey_crm_generation: Migration to version 19.0

--- survey_crm_generation/static/tests/survey_crm_generation_tour.esm.js
+++ survey_crm_generation/static/tests/survey_crm_generation_tour.esm.js
@@ -1,55 +1,62 @@
-/** @odoo-module */
-
 import {registry} from "@web/core/registry";
 
 registry.category("web_tour.tours").add("test_survey_crm_generation", {
-    test: true,
-    url: "/survey/start/08b4db21-66cc-4c69-a712-cc364c54902c",
     steps: () => [
         {
             content: "Start Survey",
             trigger: "button.btn:contains('Start Survey')",
+            run: "click",
         },
         {
             content: "E-mail address",
             trigger: "div.js_question-wrapper:contains('E-mail address') textarea",
-            run: "text test@test.com",
+            run: "edit test@test.com",
         },
         {
             content: "Continue",
             trigger: "button.btn:contains('Continue')",
+            run: "click",
         },
         {
             content: "Your company name?",
             trigger: "div.js_question-wrapper:contains('Your company name?') textarea",
-            run: "text Tecnativa",
+            run: "edit Tecnativa",
         },
         {
             content: "Continue",
             trigger: "button.btn:contains('Continue')",
+            run: "click",
         },
         {
             content: "And your name?",
             trigger: "div.js_question-wrapper:contains('And your name?') textarea",
-            run: "text Tecnativa",
+            run: "edit Tecnativa",
         },
         {
             content: "Continue",
             trigger: "button.btn.btn-primary[type='submit']",
+            run: "click",
         },
         {
             content: "Referenced by",
             trigger:
                 "div.js_question-wrapper:contains('Referenced by') span:contains('Other:')",
+            run: "click",
         },
         {
             content: "Referenced by: other",
             trigger: "div.js_question-wrapper:contains('Referenced by') textarea",
-            run: "text Mr. Odoo",
+            run: "edit Mr. Odoo",
         },
         {
             content: "Click Submit",
             trigger: "button[value='finish']",
+            run: "click",
+        },
+        {
+            content: "Modal",
+            trigger: "footer:contains('Submit') button.btn-primary",
+            run: "click",
         },
         {
             content: "Thank you",
